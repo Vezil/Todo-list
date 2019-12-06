f3d8ff597e1f35e6c68d974f686b6317486bad44
Vezil <SzymonWojaczek@wp.pl>
2019-12-06 18:30:30 +0100

    add todo class

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <img src="./assets/logo.png">
     <HelloWorld/>
+    <TodoList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld';
+import TodoList from './components/TodoList';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    TodoList
   }
 }
 </script>
 
 <style>
+
+*{
+  box-sizing:border-box;
+}
+.container{
+    max-width:600px;
+    margin: 0 auto;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+      <input type="text" class="todo-input" placeholder="What do you have to do?" v-model="newTodo" @keyup.enter="addTodo">
+      <div v-for="todo in todos" :key="todo.id" class="todo-item">
+          <div>{{todo.title}}</div>
+          <div class="delete-todo">&times</div>
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+
+  data () {
+    return {
+      newTodo: '',
+      idForTodo: 3,
+      todos:[
+      {
+          'id':1,
+          'title':'Destroy mr nasty',
+          'done':false,
+      },
+      {
+          'id':2,
+          'title':'Destroy mr pig',
+          'done':false,
+      }
+      ]
+    }
+  },
+  methods:{
+      addTodo() {
+          if(this.newTodo.trim() == 0){
+              return
+          }
+          this.todos.push({
+              id: this.idForTodo,
+              title: this.newTodo,
+              done:false,
+          })
+      
+          this.newTodo=''
+          this.idForTodo++
+      }
+  }
+}
+</script>
+
+
+<style>
+    .todo-input{
+
+        width:100%;
+        padding: 10px 18px;
+        margin-bottom: 16px;
+     
+    }
+    .todo-item{
+
+        margin-bottom: 12px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+</style>
+
