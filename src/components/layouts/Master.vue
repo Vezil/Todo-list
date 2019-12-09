9c3bbefbe7da7517e24f0c4a08a95c60b2f27f10
Vezil <SzymonWojaczek@wp.pl>
2019-12-09 23:34:46 +0100

    Add router

---
+++ src/components/layouts/Master.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="app">
+        <ul class="nav">
+            <li><router-link to="/">Home</router-link></li>
+            <li class="yourtodo"><router-link to="/todos">Todos</router-link></li>
+            <li><router-link to="/login">Login</router-link></li>
+            <li><router-link to="/register">Register</router-link></li>        
+        </ul>
+     <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+*{
+  box-sizing:border-box;
+  padding: 0;
+  margin:0;
+  font-size: 24px;
+  background-color: #567781;
+  color:aquamarine;
+}
+.container{
+    margin: 0 auto;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height:100vh;
+}
+.flex-center{
+    display: flex;
+    justify-content: center;
+}
+.nav{
+    display: flex;
+    list-style: none;
+    padding: 15px 0;
+    margin: 0;
+    justify-content: flex-end;
+    border-bottom: 1px solid rgb(179, 180, 171);
+    margin-bottom: 24px;
+    background: rgb(155, 156, 148);
+
+}
+.nav a {
+    color:#0b5fcc;
+    padding: 0 25px;
+    font-size: 14px;
+    font-weight: 600;
+    letter-spacing: .1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+    background: rgb(155, 156, 148);
+}
+li{
+    background: rgb(155, 156, 148);
+}
+.yourtodo a{
+    color:#09276b;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,36 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+import Master from './components/layouts/Master'
+import Home from './components/pages/Home'
+import Login from './components/pages/Login'
+import Register from './components/pages/Register'
 import {store} from './store/store'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
 window.eventBus = new Vue()
+
+const routes = [
+  {path:'/', component: Home},
+  {path:'/todos', component: App},
+  {path:'/login', component: Login},
+  {path:'/register', component: Register},
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+})
 
 /* eslint-disable no-new */
 new Vue({
   
   el: '#app',
+  router: router,
   store: store,
-  components: { App },
-  template: '<App/>'
+  components: { Master },
+  template: '<Master/>'
 })
